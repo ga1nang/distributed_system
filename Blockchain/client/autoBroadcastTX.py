864a8503c0633d7120d36eb30179f568474693d0
ga1nang <nnthanhmx@gmail.com>
2024-06-08 15:35:17 +0700

    test6

--- Blockchain/client/autoBroadcastTX.py
+++ Blockchain/client/autoBroadcastTX.py
@@ -1,12 +1,21 @@
 import requests
 import time
 import random
+import configparser
 from Blockchain.Backend.core.database.database import AccountDB
 
 fromAccount = "1LYgXwYXw16GJXgDwHV7aCNijnQWYEdc1C"
 
 """ Read all the accounts """
 AllAccounts = AccountDB().read()
+
+config = configparser.ConfigParser()
+config.read('config.ini')
+
+# Get the Webhost details
+webhost = config['Webhost']['host']
+webport = config['Webhost']['port']
+url = f"http://{webhost}:{webport}/wallet"
 
 def autoBroadcast():
     while True:
@@ -16,5 +25,5 @@ def autoBroadcast():
                         "toAddress": account["PublicAddress"],
                         "Amount": random.randint(1,35)}
 
-                res = requests.post(url ="http://localhost:5900/wallet", data = paras)   
+                res = requests.post(url, data = paras)   
         time.sleep(2)
